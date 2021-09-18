abfabba8a1ae9764a64881d4e950828b50fc0d09
EvgeniyRashow <hakerovski@yandex.ru>
2021-09-18 12:41:01 +0300

    First

---
+++ dist/js/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+  var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  $(".prices__parent").click(function () {        
+    $(this).children(".child").slideToggle(300);
+});
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+$(document).ready(function(){
+  $('.slaider__inner').slick({
+      autoplay: true,
+      autolaySpeed: 2000,
+      prevArrow:'<button type="button" class="slick-prev"><img src="../icons/leftright/ligtleft.png"></button>',
+      nextArrow:'<button type="button" class="slick-next"><img src="../icons/leftright/ligtright.png"></button>',
+  });
+   
+});
+
