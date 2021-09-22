acb40a5914a0fd48f7171764c5609480894ad722
EvgeniyRashow <hakerovski@yandex.ru>
2021-09-22 13:11:13 +0300

    adaption

--- dist/js/script.js
+++ dist/js/script.js
@@ -41,4 +41,11 @@ $(window).scroll(function() {
     $('.pageup').fadeOut();
   }
 });
+$(function(){
+  $("a[href^='#']").click(function(){
+          var _href = $(this).attr("href");
+          $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+          return false;
+  });
+});
 
